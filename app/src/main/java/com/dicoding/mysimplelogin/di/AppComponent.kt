86b517733525941c39ccfd5c1f6e1da322da0f12
Arif <faizin.arief@gmail.com>
2023-03-03 10:29:13 +0700

    Update indentation

--- app/src/main/java/com/dicoding/mysimplelogin/di/AppComponent.kt
+++ app/src/main/java/com/dicoding/mysimplelogin/di/AppComponent.kt
@@ -10,11 +10,11 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [StorageModule::class])
 interface AppComponent {
-   @Component.Factory
-   interface Factory {
-       fun create(@BindsInstance context: Context): AppComponent
-   }
- 
-   fun inject(activity: MainActivity)
-   fun inject(activity: HomeActivity)
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    fun inject(activity: MainActivity)
+    fun inject(activity: HomeActivity)
 }
